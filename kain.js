f998aacc5a09f3439813f232c5ef1f0709ce8576
adeyemiadedayo@ <adeyemiadedayo@>
2025-01-05 12:32:52 +0100

    first commit

---
+++ kain.js
@@ -0,0 +1,28 @@
+
+let name = "John Doe";
+
+let age = 30;
+
+let isActive = true;
+
+let colors = ["red", "blue", "green"];
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+};
+
+let greet = function() {
+    console.log("Hello, welcome!");
+};
+
+let emptyValue = null;
+
+let undefinedValue;
+
+let uniqueId = Symbol("id");
+
+let largeNumber = 30n;
+
+let currentDate = new Date();
---
+++ main.js
@@ -0,0 +1,13 @@
+const person = {
+    firstName: 'john',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+console.log(person);
